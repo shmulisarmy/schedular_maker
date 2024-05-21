d00f4a7c03ad84d9dc8f803ada1ba597d9e7492c
shmuli <shmulikeller@gmail.com>
2024-05-21 17:12:28 -0400

    switch_to_flask_to_deploy_on_python_anywhere

--- main.py
+++ main.py
@@ -1,49 +1,13 @@
-from fastapi import FastAPI, Depends, HTTPException, status, Request, APIRouter
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
-from typing import List, Optional
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
+from flask import Flask, render_template, request
 
-
-app = FastAPI()
-templates = Jinja2Templates(directory='templates')
-
-# static folder
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
-def home(request: Request):
-    return templates.TemplateResponse('main.html', {'request': request})
-
-
-@app.route('/test')
-def home(request: Request):
-    return templates.TemplateResponse('test.html', {'request': request})
+def home():
+    return render_template('main_page.html')
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-link = 'http://127.0.0.1:8000'
-
-
-for route in app.routes[4:]:
-    print(f'{link}{route.path }')
-#run command: uvicorn main:app --reload
+if __name__ == '__main__':
+    app.run(debug=True)
